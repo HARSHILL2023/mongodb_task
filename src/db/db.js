b24c1d1db0fe76c7c25bf016d28065f1077639fe
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-03-06 11:26:42 +0530

    Prepare for render deploy

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
- async function database(){
-    await mongoose.connect("mongodb://localhost:27017/Task") 
+async function database() {
+   await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/Task")
 
 
-    console.log("mongodb is connected")
- }
+   console.log("mongodb is connected")
+}
 
 
- module.exports=database
+module.exports = database
